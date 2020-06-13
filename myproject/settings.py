8c3b6b18fd602667785daa7adc545b61a3b0cacd
chandanmanna48 <chandanmanna48@gmail.com>
2020-06-13 23:39:22 +0530

    debug false

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ah)t2x@%#v4re9rx3f0rpt%ve=iw&vx(#eu9tppe9#lger=ay+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
